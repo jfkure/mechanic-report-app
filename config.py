d7429af47b395891266456a39fc9715e0801d134
jfkure <jfkure@gmail.com>
2025-07-12 22:50:14 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SECRET_KEY = 'your-secret-key'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
