11c493ace27e921eda1243bd35211479ec517478
jfkure <jfkure@gmail.com>
2025-07-12 23:40:26 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 # The new entry point to start your app
+import os
 from app import create_app, db
+
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to the port Render provides (default to 5000 if not found)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
