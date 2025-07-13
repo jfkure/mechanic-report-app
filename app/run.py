dd3db4f111a9a7ea0f3de36b7ec8304b20d1ba17
jfkure <jfkure@gmail.com>
2025-07-12 22:48:03 -0400

    Delete app/run.py

--- app/run.py
+++
@@ -1,9 +0,0 @@
-# The new entry point to start your app
-from app import create_app, db
-app = create_app()
-
-with app.app_context():
-    db.create_all()
-
-if __name__ == '__main__':
-    app.run(debug=True)
